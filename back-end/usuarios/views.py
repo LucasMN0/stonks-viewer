91a15d62b5e430ab4dca3f0ab2ad54aafaf9552f
art <amaralsouza0202@gmail.com>
2025-10-17 21:18:15 -0300

    login feito

--- back-end/usuarios/views.py
+++ back-end/usuarios/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from .forms import LoginForm
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'Bem-vindo, {user.first_name}!')
+                return redirect('perfil')  # redireciona para a página de perfil
+            else:
+                messages.error(request, 'E-mail ou senha inválidos.')
+    else:
+        form = LoginForm()
+
+    return render(request, 'usuarios/index.html', {'form': form})
 
 
 def cadastro(request):
