460d30074d4907b3fef9fd3bc9e33cc36979c449
art <amaralsouza0202@gmail.com>
2025-10-30 02:24:09 -0300

    adicionei coisa pra caramba n lembro de tudo, porem ta tudo relacionado ao teamplate perfil

--- back-end/usuarios/models.py
+++ back-end/usuarios/models.py
@@ -1,44 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-class Transacao(models.Model):
-    TIPO_CHOICES = [
-        ('expense', 'Gasto fixo'),
-        ('extra', 'Gasto extra'),
-        ('income', 'Ganho fixo'),
-        ('investment', 'Ganho extra'),
-        ('other', 'Outro'),
-    ]
-    class Meta:
-        verbose_name = 'Transação'
-        verbose_name_plural = 'Transações'
-
-    data = models.DateField()
-    descricao = models.CharField(max_length=255)
-    tipo = models.CharField(max_length=20, choices=TIPO_CHOICES)
-    valor = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return f"{self.descricao} - {self.valor}"
-
-
-class MetaFinanceira(models.Model):
-    nome = models.CharField(max_length=255)
-    valor = models.DecimalField(max_digits=10, decimal_places=2)
-    concluida = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.nome
-
-
-class Lembrete(models.Model):
-    nome = models.CharField(max_length=255)
-    descricao = models.TextField(blank=True)
-    data = models.DateField()
-
-    def __str__(self):
-        return self.nome
-
 
 class CustomUserManager(BaseUserManager):
     def create_user(self,email,password=None,first_name=None, **extra_fields):
@@ -74,3 +36,54 @@ class CustomUser(AbstractBaseUser,PermissionsMixin):
         verbose_name_plural = 'Usuarios'
     def __str__(self):
         return self.email
+
+
+class Transacao(models.Model):
+    TIPO_CHOICES = [
+        ('income', 'Ganho fixo'),
+        ('investment', 'Ganho extra'),
+        ('expense', 'Gasto fixo'),
+        ('extra', 'Gasto extra'),
+        ('other', 'Outro'),
+    ]
+
+    usuario = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='transacoes')
+    data = models.DateField()
+    descricao = models.CharField(max_length=255)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    tipo = models.CharField(max_length=20, choices=TIPO_CHOICES)
+
+    class Meta:
+        verbose_name = 'Transação'
+        verbose_name_plural = 'Transações'
+
+    def __str__(self):
+        return f"{self.descricao} - {self.valor}"
+
+
+class MetaFinanceira(models.Model):
+    usuario = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='metas')
+    nome = models.CharField(max_length=255)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, default='Pendente')
+
+    class Meta:
+
+        verbose_name = 'Meta Financeira'
+        verbose_name_plural = 'Metas Financeiras'
+
+    def __str__(self):
+        return self.nome
+
+
+class Lembrete(models.Model):
+    usuario = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='lembretes')
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField(blank=True)
+    data = models.DateField()
+    class Meta:
+        verbose_name = 'Lembrete'
+        verbose_name_plural = 'Lembretes'
+
+    def __str__(self):
+        return self.nome
